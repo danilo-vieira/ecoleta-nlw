49c808f0cec7c9622955c3d080932e82c678ecd8
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-06-01 11:56:59 -0300

    Initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+  return response.json({ message: 'Hello NLW!' });
+})
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333!');
+});
